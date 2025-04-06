2a94ab44e6dca508c94a6b8cbb19fb9fe30d8daa
sumit6387 <sumitkumar993618@gmail.com>
2025-04-06 13:10:21 +0530

    feat: added linting and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    node: true,
+    es2020: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    // Customize rules here
+  },
+};
